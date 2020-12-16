859966cb1de473a94f9880fe965a175c681db024
ColdIV <yufoolme@gmail.com>
2020-12-16 15:02:50 +0100

    move player with wasd

--- inc/update.cpp
+++ inc/update.cpp
@@ -1,6 +1,26 @@
 #include "Game.h"
-
+#include <iostream>
 void Game::update () {
+    SDL_Point move = { 0, 0 };
+    int32_t speed = this->player.getSpeed();
+    const Uint8 *keystates = SDL_GetKeyboardState(NULL);
+
+    if (keystates[SDL_SCANCODE_W]) {
+        move.y += -1 * speed;
+    }
+    if (keystates[SDL_SCANCODE_A]) {
+        move.x += -1 * speed;
+    }
+    if (keystates[SDL_SCANCODE_S]) {
+        move.y += 1 * speed;
+    }
+    if (keystates[SDL_SCANCODE_D]) {
+        move.x += 1 * speed;
+    }
+
+    // Move according to pressed buttons
+    this->player.move(move.x, move.y);
+
     // Handle events
     while (SDL_PollEvent(&(this->event))) {
         switch (event.type) {
